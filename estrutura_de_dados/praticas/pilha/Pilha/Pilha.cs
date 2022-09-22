2ebf21973a894d5904e4b2756c61f3e47951e3ad
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-22 12:45:39 -0300

    Completando o programa.cs para uso

--- estrutura_de_dados/praticas/pilha/Pilha/Pilha.cs
+++ estrutura_de_dados/praticas/pilha/Pilha/Pilha.cs
@@ -8,32 +8,38 @@ namespace Pilha
 {
     internal class Pilha
     {
+        //Declarando o vetor e a variável topo
         private int[] vetor_pilha;
         private int topo;
 
+        // Criando o construtor
         public Pilha(int tamanho)
         {
             vetor_pilha = new int[tamanho];
             topo = 0;
         }
 
+        // Método para empilhar
         public void Empilhar(int item)
         {
             vetor_pilha[topo] = item;
             topo++;
         }
 
+        //Método para desempilhar
         public int Desempilhar()
         {
             topo--;
-            return topo;
+            return (vetor_pilha[topo]);
         }
 
+        //Método para avisar preenchimento total da pilha
         public bool Cheia()
         {
             return (topo == vetor_pilha.Length);
         }
 
+        //Método para avisar que a pilha está vazia
         public bool Vazia()
         {
             return (topo == 0);
--- estrutura_de_dados/praticas/pilha/Pilha/Program.cs
+++ estrutura_de_dados/praticas/pilha/Pilha/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,145 @@ namespace Pilha
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            /*A string vai receber o valor numérico
+              para tratar erro do usuário digitar letra
+              espaço ou somente pressionar enter*/
+            string tamanhoPilha;
+            string numerico;
+            
+            //Variáveis para receber os valores das strings
+            int numero;
+            int tamanhoPilhaNumerico;
+
+            //Variável booleana para while da pilha
+            bool validar = true;        
+
+            //Ditando o tamanho da pilha
+            Console.Write("Primeiro digite o tamanho da pilha que deseja usar: ");
+            tamanhoPilha = Console.ReadLine();
+
+            while (!tamanhoPilha.All(char.IsDigit) || tamanhoPilha == " " || tamanhoPilha == "")
+            {
+                // Looping para tratar erro de caracter inesperado
+                Console.Clear();
+                Console.Write("Por favor digite um valor numérico para ser o tamanho da pilha: ");
+                tamanhoPilha = Console.ReadLine();
+            }
+
+            tamanhoPilhaNumerico = int.Parse(tamanhoPilha);
+            Pilha p = new Pilha(tamanhoPilhaNumerico);
+            Console.Clear();
+
+            while (validar)
+            {
+                Opcoes();
+                numerico = Console.ReadLine();
+
+                while(!numerico.All(char.IsDigit) || numerico == " " || numerico == "")
+                {
+                    // Looping para tratar erro de caracter inesperado
+                    Console.Clear();
+                    Console.WriteLine("Linha de comando não encontrada.\nPor favor, digite uma das opções abaixo: \n");
+                    Opcoes();
+                    numerico = Console.ReadLine();
+                }
+                
+                numero = int.Parse(numerico);
+
+                if(numero >= 0 && numero <= 3)
+                {
+                    // Empilhar valor se não tiver cheia
+                    if (numero == 1)
+                    {
+                        if (!p.Cheia())
+                        {
+                            Console.Clear();
+                            int ValorEmpilhar;
+                            Console.WriteLine("Digite o valor a ser empilhado: ");
+                            ValorEmpilhar = Convert.ToInt32(Console.ReadLine());
+                            p.Empilhar(ValorEmpilhar);
+
+                            Console.Clear();
+                            Console.WriteLine($"Você empilhou o valor {p.Desempilhar()}!");
+                            p.Empilhar(ValorEmpilhar);
+
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Pilha Cheia! Desempilhe um valor para empilhar novamente!");
+                        }
+                    }
+                    else if (numero == 2) //Desempilha um único valor e mostra na tela
+                    {
+                        if (p.Vazia())
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Por favor, coloque um valor na pilha!");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Você desempilhou o valor {p.Desempilhar()} !");
+                            Console.Write("\n\nPressione qualquer tecla para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+
+                    }
+                    else if (numero == 3) //Desempilha e imprime todos valores
+                    {
+                        if (!p.Cheia())
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Por favor, preencha a pilha primeiro!");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Desempilhar Tudo: ");
+                            for (int i = 0; i < tamanhoPilhaNumerico; i++)
+                            {
+                                Console.WriteLine(p.Desempilhar());
+                            }
+                            Console.WriteLine("\n***Você imprimiu os valores empilhados!***");
+                            Console.Write("\n\nPressione qualquer tecla para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+
+                    }
+                    else if (numero == 0) //Sai do programa
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Você finalizou o programa!");
+                        validar = false;
+                    }
+
+                }else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida/Inexistente!");
+                }
+                
+
+
+            }
+            Console.WriteLine("\n\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
+
+        static void Opcoes()
+        {
+            Console.WriteLine("Escolha uma opção: \n");
+            Console.WriteLine("1 - Empilhar");
+            Console.WriteLine("2 - Desempilhar e Imprimir um valor");
+            Console.WriteLine("3 - Desempilhar e Imprimir tudo");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Digite aqui: ");
+        }
+
     }
 }
