62f90402aa44a2deb89d713c3a66ef4f654d2c58
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2022-09-22 09:33:54 -0300

    Adicionar arquivos de projeto.

---
+++ estrutura_de_dados/praticas/pilha/Pilha/Pilha.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha
+{
+    internal class Pilha
+    {
+        private int[] vetor_pilha;
+        private int topo;
+
+        public Pilha(int tamanho)
+        {
+            vetor_pilha = new int[tamanho];
+            topo = 0;
+        }
+
+        public void Empilhar(int item)
+        {
+            vetor_pilha[topo] = item;
+            topo++;
+        }
+
+        public int Desempilhar()
+        {
+            topo--;
+            return topo;
+        }
+
+        public bool Cheia()
+        {
+            return (topo == vetor_pilha.Length);
+        }
+
+        public bool Vazia()
+        {
+            return (topo == 0);
+        }
+    }
+}
